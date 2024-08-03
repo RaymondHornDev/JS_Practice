9a7a874e26d20777646abd29467fe9475ddeff26
RaymondHornDev <132532625+RaymondHornDev@users.noreply.github.com>
2024-08-02 22:48:32 -0400

    Update

--- ListSort/ListSort.js
+++ ListSort/ListSort.js
@@ -1,5 +1,22 @@
-let node = {
-  value: null,
-  left: null,
-  right: null,
+node_maker = function (value) {
+  let node = {
+    value: value,
+    left: null,
+    right: null,
+  };
+  return node;
+};
+
+let list = {
+  head: null,
+  tail: null,
+  has_nodes: false,
+
+  push: function (value) {
+    if (this.has_nodes == false) {
+      this.head = node_maker(value);
+      this.tail = this.head;
+      this.has_nodes = true;
+    }
+  },
 };
